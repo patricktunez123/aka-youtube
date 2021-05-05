9736d888e7c6c3eee5503677825fad29094f6c93
Patrick <tunezepatrick@gmail.com>
2021-05-05 10:44:50 +0200

    design categories bar

--- src/components/CategoriesBar/CategoriesBar.js
+++ src/components/CategoriesBar/CategoriesBar.js
@@ -1,8 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./_categoriesBar.scss";
 
+const categories = [
+  "item 1",
+  "item 2",
+  "item 3",
+  "item 4",
+  "item 5",
+  "item 6",
+  "item 7",
+  "item 8",
+  "item 9",
+  "item 10",
+  "item 11",
+  "item 12",
+  "item 13",
+  "item 14",
+  "item 15",
+  "item 16",
+  "item 17",
+  "item 18",
+  "item 19",
+  "item 20",
+  "item 8",
+  "item 9",
+  "item 10",
+  "item 11",
+  "item 12",
+  "item 13",
+  "item 14",
+  "item 15",
+  "item 16",
+  "item 17",
+  "item 18",
+  "item 19",
+  "item 20",
+  "item 8",
+  "item 9",
+  "item 10",
+  "item 11",
+  "item 12",
+  "item 13",
+  "item 14",
+  "item 15",
+  "item 16",
+  "item 17",
+  "item 18",
+  "item 19",
+  "item 20",
+];
+
 const CategoriesBar = () => {
-  return <div>CategoriesBar</div>;
+  const [activeCategy, setActiveItem] = useState("item 1");
+  const handleClick = (value) => {
+    setActiveItem(value);
+  };
+
+  return (
+    <div className="categoriesBar">
+      {categories.map((category, index) => (
+        <span
+          key={index}
+          onClick={() => handleClick(category)}
+          className={activeCategy === category ? "active" : ""}
+        >
+          {category}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default CategoriesBar;
